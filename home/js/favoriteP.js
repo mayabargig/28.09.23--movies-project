b1e71aa92752549798a8078414346a8d243cc49c
mayabargig <mayabargig@gmail.com>
2023-12-30 16:12:27 +0200

    update

--- home/js/favoriteP.js
+++ home/js/favoriteP.js
@@ -1,34 +1,62 @@
-const favoriteMovies = JSON.parse(localStorage.getItem("favoriteArray"));
 
-function displayFavoriteMovies() {
-    
-    moviesContainer.innerHTML = "";
-    
-    if (favoriteMovies.length === 0) {
-        moviesContainer.innerHTML += "<p>You haven't added any favorite movies yet.</p>";
-    } else {
-        favoriteMovies.forEach((movie) => {
-            const movieCard = document.createElement("div");
-            movieCard.classList.add("movie-card");
-            movieCard.innerHTML += movie;
-            moviesContainer.appendChild(movieCard);
-                });
-            }
-        }
 
-        displayFavoriteMovies();
+if (!favoriteArray || favoriteArray.length === 0) {
+    moviesContainer.innerHTML = "<p>You haven't added any favorite movies yet.</p>";
+} else {
+    favoriteArray.forEach((movie) => {
+        fetchFavoriteMovies(movie);
+    });
+};
 
-    // const removeButtons = favoritesContainer.querySelectorAll(".remove-favorite-button");
 
-    // removeButtons.forEach((button) => {
-    //     button.addEventListener("click", function () {
-    //         const movieCard = button.closest(".movie-card");
-    //         const movieTitle = movieCard.querySelector("h3").textContent;
+function fetchFavoriteMovies(movieId) {
+    let url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`;
 
-    //         favoriteMovies = favoriteMovies.filter((movie) => movie.title !== movieTitle);
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const movieDetails = data;
+            console.log(movieDetails);
+            displayFavoriteMovies(movieDetails);
+        })
+        .catch((error) => console.error("Error fetching movie details:", error));
+};
 
-    //         localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
 
-    //         displayFavoriteMovies();
-    //     });
-    // });
+function displayFavoriteMovies(movie) {
+    const posterPath = movie.poster_path
+        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        : "no-image-available.jpg";
+    const isLiked = favoriteArray.includes(movie.id + '');
+    const heartClass = isLiked ? "fas fa-heart" : "far fa-heart";
+
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie-card");
+
+    movieCard.innerHTML += `
+            <img class="movie-poster" src="${posterPath}" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+            <p>Release Date: ${movie.release_date}</p>
+            <i class="${heartClass}" id="${movie.id}" onclick="changeColor(this)"></i>`;
+
+    moviesContainer.appendChild(movieCard);
+};
+
+
+/** TODO: fetch the api to get the movie by the id than push it after create the element */
+// });
+
+// const removeButtons = favoritesContainer.querySelectorAll(".remove-favorite-button");
+
+// removeButtons.forEach((button) => {
+//     button.addEventListener("click", function () {
+//         const movieCard = button.closest(".movie-card");
+//         const movieTitle = movieCard.querySelector("h3").textContent;
+
+//         favoriteMovies = favoriteMovies.filter((movie) => movie.title !== movieTitle);
+
+//         localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
+
+//         displayFavoriteMovies();
+//     });
+// });
--- home/js/main.js
+++ home/js/main.js
@@ -1,75 +1,72 @@
-const apiKey = "be93d2314b628d913609d274fc1bdf70";
 const moviesContainer = document.getElementById("moviesContainer");
 const popularitySelect = document.getElementById("popularitySelect");
 const pagination = document.getElementById("pagination");
 
-let currentPage = 1;
-
-popularitySelect.addEventListener("change", fetchMovies);
-pagination.addEventListener("click", handlePaginationClick);
-
-function fetchMovies() {
-    const popularity = popularitySelect.value;
-    console.log(popularity);
-    const url = `https://api.themoviedb.org/3/trending/movie/${popularity}?api_key=${apiKey}&language=en-US&page=${currentPage}`;
-    
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        const movies = data.results;
-        console.log(movies);
-        displayMovies(movies);
-    })
-    .catch((error) => console.error("Error fetching movies:", error));
+function setF(favoriteArray) {
+    localStorage.setItem("favoriteArray", JSON.stringify(favoriteArray));
 }
 
-let favoriteArray = [];
-let movieId = 0;
+let currentPage = 1;
+
+popularitySelect ? popularitySelect.addEventListener("change", fetchMovies) : null;
+pagination ? pagination.addEventListener("click", handlePaginationClick) : null;
+
+
+function fetchMovies() {
+    const popularity = popularitySelect?.value;
+    console.log(popularity);
+    const url = `https://api.themoviedb.org/3/trending/movie/${popularity}?api_key=${apiKey}&language=en-US&page=${currentPage}`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const movies = data.results;
+            console.log(movies);
+            displayMovies(movies);
+        })
+        .catch((error) => console.error("Error fetching movies:", error));
+}
+
 
 function displayMovies(movies) {
     moviesContainer.innerHTML = "";
-    
+
     movies.forEach((movie) => {
-        movieId++;
+        // TODO: convert to a function and move to shared file...
+        // use it from both main and fav page
+        const isLiked = favoriteArray?.includes(movie?.id + '')
+        const likedIcon = 'far fa-heart'
+        const unlikeIcon = 'fas fa-heart'
+        const heartClass = isLiked ? likedIcon : unlikeIcon;
+
         const movieCard = document.createElement("div");
         movieCard.classList.add("movie-card");
-        
+
         const posterPath = movie.poster_path
-        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-        : "no-image-available.jpg"; 
-        
+            ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+            : "no-image-available.jpg";
+
         movieCard.innerHTML += `
         <img class="movie-poster" src="${posterPath}" alt="${movie.title}">
         <h3>${movie.title}</h3>
         <p>Release Date: ${movie.release_date}</p>
-        <div class="favoriteButton" id="redButton-${movieId}" onclick="changeColor(this)"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-        </svg> </div>
-        `;
+        <i class="${heartClass}" id="${movie.id}" onclick="changeColor(this)"></i>`;
 
-        favoriteArray.push(movieCard.innerHTML);
         moviesContainer.appendChild(movieCard);
         createPagination();
     });
     console.log(favoriteArray);
-    localStorage.setItem("favoriteArray", JSON.stringify(favoriteArray));
 }
 
-function changeColor (button){
-        if(button.style.color !== "red"){
-            button.style= "color: red";
-        }else{
-            button.style= "color: black";
-        } 
- }
+
 
 function createPagination() {
-    pagination.innerHTML ="";
-    for (let i = 1; i <= 5; i++){
+    pagination.innerHTML = "";
+    for (let i = 1; i <= 5; i++) {
         const button = document.createElement("button");
         button.textContent = i;
-        
-        if(i === currentPage){
+
+        if (i === currentPage) {
             button.classList.add("selected")
         }
         button.addEventListener("click", () => {
